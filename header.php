aae523f5ae67d1248f288c492c46fd471831db6b
angelica1811 <77701214+angelica1811@users.noreply.github.com>
2023-03-30 14:40:46 -0600

    fixed header

--- header.php
+++ header.php
@@ -18,6 +18,9 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
 	<style>
 			.themecolorone{
 			background-color: #A5C2A6;
@@ -35,7 +38,6 @@
 				
 			}
 		</style>
-<link href="https://fonts.cdnfonts.com/css/shamson-signature" rel="stylesheet">
                 
 	
 	<?php wp_head(); ?>
